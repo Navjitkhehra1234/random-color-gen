79690c3156575eeb9d12adec7606e4f57d0d77a8
Navjit Khehra <793809@ACDM.DS.SAIT.CA>
2018-10-17 13:38:31 -0600

    Initial commit

---
+++ mycode.js
@@ -0,0 +1,12 @@
+function getNewColor() {
+    var symbols = "0123456ABCDEF";
+    color = "#";
+    for(var i= 0; i<6; i++) {
+        color = color + symbols[Math.floor(Math.random) * 16];
+    }
+    document.body.style.background = color;
+    document.getElementById("hex").innerHTML = color;
+
+}
+getNewColor();       
+
